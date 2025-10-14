eadc896d76905b91a65b808212245f532fd262ea
QuisTech <michquis@gmail.com>
2025-10-14 15:00:50 +0100

    still troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
-import CandidatesPage from "./pages/CandidatesPage"; // Add this
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/candidates" element={<CandidatesPage />} />
-        <Route path="/interviews" element={<CandidatesPage />} /> {/* Reuse for now */}
-        <Route path="/settings" element={<CandidatesPage />} /> {/* Reuse for now */}
-      </Routes>
-    </Router>
+    <div style={{ minHeight: '100vh', backgroundColor: '#f3f4f6' }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
